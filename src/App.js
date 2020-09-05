53144e65d861d2be1c0bd72a2b7a5e2d670d7b1e
Gariel Giles <gariel@Gariels-MacBook-Air.local>
2020-09-05 18:25:54 -0500

    added constants to begin connecting back end

--- src/App.js
+++ src/App.js
@@ -12,8 +12,54 @@ const App = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   useEffect(() => {
-    
-  })
+    if (localStorage.token) {
+      setIsLoggedIn(true);
+    } else {
+      setIsLoggedIn(false);
+    }
+  }, [isLoggedIn]);
+
+  const handleSignUp = async (event) => {
+    event.preventDefault();
+    try {
+      const response = await axios.post("http://localhost:3001/users/signup", {
+        email: state.email,
+        password: state.password,
+      });
+      console.log(response);
+      localStorage.token = response.data.token;
+      setIsLoggedIn(true);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  const handleLogIn = async (event) => {
+    event.preventDefault();
+    try {
+      const response = await axios.post("", {
+        email: state.email,
+        password: state.password
+      });
+      localStorage.token = response.data.token;
+      setIsLoggedIn(true);
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  const handleLogOut = () => {
+    setState({
+      email: "",
+      password: "",
+      isLoggedIn: false
+    });
+    localStorage.clear();
+  };
+
+  const handleInput = (event) => {
+    setState({ ...state, [event.target.name]: event.target.value })
+  }
 
 }
 
