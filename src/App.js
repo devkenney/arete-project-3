4db58d82f1862bab6985554ebd1234451154a87d
Devin Kenney <devin.kenney111@gmail.com>
2020-09-05 20:07:01 -0400

    accidentally overwrote the app js file so fixing it

--- src/App.js
+++ src/App.js
@@ -1,27 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
-import "bootswatch/dist/sketchy/bootstrap.min.css"; 
+import React, { useState, useEffect } from 'react';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+  const [state, setState] = useState({
+    email: "",
+    password: "",
+    isLoggedIn: false
+  });
+
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    if (localStorage.token) {
+      setIsLoggedIn(true);
+    } else {
+      setIsLoggedIn(false);
+    }
+  }, [isLoggedIn]);
+
+  const handleSignUp = async (event) => {
+    event.preventDefault();
+    try {
+      const response = await axios.post("http://localhost:3001/users/signup", {
+        email: state.email,
+        password: state.password,
+      });
+      console.log(response);
+      localStorage.token = response.data.token;
+      setIsLoggedIn(true);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  const handleLogIn = async (event) => {
+    event.preventDefault();
+    try {
+      const response = await axios.post("", {
+        email: state.email,
+        password: state.password
+      });
+      localStorage.token = response.data.token;
+      setIsLoggedIn(true);
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  const handleLogOut = () => {
+    setState({
+      email: "",
+      password: "",
+      isLoggedIn: false
+    });
+    localStorage.clear();
+  };
+
+  const handleInput = (event) => {
+    setState({ ...state, [event.target.name]: event.target.value })
+  }
+
 }
+
 
 export default App;
