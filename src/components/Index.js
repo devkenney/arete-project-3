25717a844236756c33ecdc9e30949f9ce7206065
Gariel Giles <gariel@Gariels-MacBook-Air.local>
2020-09-08 11:09:36 -0500

    added ComicShow page and function to Index

--- src/components/Index.js
+++ src/components/Index.js
@@ -1,11 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import ComicShow from "./ComicShow";
 
 function Index(props) {
-    return (
-        <div>
-            <h1>This is Index</h1>
-        </div>
-    );
+    const [comics, setComics] = useState([]);
+
+    useEffect(() => {
+        async function fetchData() {
+            const response = await axios.get("/marvel/comics");
+            setComics(response.data.data.results);
+        }
+        fetchData();
+    }, [comics]);
+
+    const showComics = comics.map((comic, i) => {
+        return (
+            <div key={i}>
+                <ComicShow comic={comic} />
+            </div>
+        )
+    })
+    return <div>{showComics}</div>
 }
 
 export default Index;
